704fd91d2dd11adda6c133ed397f0bb00f8629a2
PRASHANTH S N <prashanthsn2003@gmail.com>
2025-02-25 11:07:48 +0530

    Updating API Key

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const deleteChatButton = document.querySelector("#delete-chat-button");
 let userMessage = null;
 let isResponseGenerating = false; 
 //API configuration
-const API_KEY = "AIzaSyBzGH0mJIGEVOWGRJYlcyCJz0fJrR0DXf4";
+//AIzaSyBzGH0mJIGEVOWGRJYlcyCJz0fJrR0DXf4
+//AIzaSyCP6Wqvj-RHqPeX969dstPUUtxkRo-GejU
+const API_KEY = "AIzaSyDeuCq6_tOVa5XYtve8IGyF4TJfmZp8dv4";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 
